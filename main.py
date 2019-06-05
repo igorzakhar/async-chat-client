0d26f68d2e1d36b7bdc116ed4a73c7db78819569
igorzakhar <igorzakhar@gmail.com>
2019-06-05 11:53:44 +0300

    Add reconnect to chat server if connection is lost

--- main.py
+++ main.py
@@ -2,25 +2,53 @@ import asyncio
 import argparse
 
 
-async def tcp_client(host, port):
-    reader, _ = await asyncio.open_connection(host, port)
+async def _get_chat_connection(host, port, attempts=1, timeout=3):
+    attempts_count = 0
+    reader = None
+    while not reader:
+        try:
+            reader, _ = await asyncio.open_connection(host, port)
+            print('Установлено соединение.')
+        except (ConnectionRefusedError, ConnectionResetError) as err:
+            if attempts_count < attempts:
+                print('Нет соединения. Повторная попытка.')
+                attempts_count += 1
+                continue
+            else:
+                print(
+                    f'Нет соединения. Повторная попытка через {timeout} сек.'
+                )
+                await asyncio.sleep(timeout)
+    return reader
 
+
+async def _read_chat(reader):
     while True:
         data = await reader.readline()
         print(data.decode().strip())
 
 
+async def main():
+    args = process_args()
+    while True:
+        try:
+            reader = await _get_chat_connection(args.host, args.port)
+            await _read_chat(reader)
+        except (ConnectionRefusedError, ConnectionResetError) as err:
+            print('Соединение потеряно.')
+            continue
+
+
 def process_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-H', '--host')
-    parser.add_argument('-P', '--port')
+    parser.add_argument('-H', '--host', help='Server address.')
+    parser.add_argument('-P', '--port', help='Server port.')
 
     return parser.parse_args()
 
 
 if __name__ == '__main__':
-    args = process_args()
     try:
-        asyncio.run(tcp_client(args.host, args.port))
+        asyncio.run(main())
     except KeyboardInterrupt:
         print('\nKeyboard Interrupt')
